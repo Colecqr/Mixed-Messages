18e55fa1a452fdd34e0b389b0ac6adf617611f7e
Colecar <colecarcat@gmail.com>
2024-07-19 14:38:19 -0700

    added an array of silly adjectives

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // this factory function creates recipient profiles
-
 const profile = (name, nickname, interest, grade) => {
     return {
         name, 
@@ -8,3 +7,6 @@ const profile = (name, nickname, interest, grade) => {
         grade,
     }
 }
+
+// this array contains a variety of wacky adjetives
+const adjectives15 = ['hilarious', 'merry', 'silly', 'amusing', 'diverting', 'entertaining', 'comical', 'whimsical', 'droll', 'riotous', 'jocular', 'waggish', 'hysterical', 'antic', 'risible']
