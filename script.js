b1e3c47cb3de0d900df5d3e57085407e58e858b4
Colecar <colecarcat@gmail.com>
2024-07-19 14:29:35 -0700

    created factory function for recipient profiles

---
+++ script.js
@@ -0,0 +1,10 @@
+// this factory function creates recipient profiles
+
+const profile = (name, nickname, interest, grade) => {
+    return {
+        name, 
+        nickname, 
+        interest, 
+        grade,
+    }
+}
